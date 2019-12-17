da560cc26292c7cef24c645939870b4911199425
Jack Ferguson <jack.ferguson552@gmail.com>
2019-12-17 18:02:38 +0000

    made a bit of staffing

---
+++ ThAmCo.Events/Controllers/StaffingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Controllers
+{
+    public class StaffingsController : Controller
+    {
+
+        private readonly EventsDbContext _context;
+
+        public StaffingsController(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ ThAmCo.Events/Data/Staffings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Events.Data
+{
+    public class Staffings
+    {
+        [Key, Column(Order = 0)]
+        public int StaffId { get; set; }
+
+        public Staff Staff { get; set; }
+
+        public int EventId { get; set; }
+
+        public Event Event { get; set; }
+             
+
+    }
+}
