528509392c0b9d29e8a16dc9b3a42b5c09ed1d7c
Jack Ferguson <jack.ferguson552@gmail.com>
2019-12-17 17:25:04 +0000

    staffing table created

---
+++ ThAmCo.Events/Data/Migrations/20191217172205_update-database.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThAmCo.Events.Data.Migrations
+{
+    public partial class updatedatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Staffs",
+                schema: "thamco.events",
+                columns: table => new
+                {
+                    StaffId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    Surname = table.Column<string>(nullable: false),
+                    FirstAider = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staffs", x => x.StaffId);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "thamco.events",
+                table: "Staffs",
+                columns: new[] { "StaffId", "FirstAider", "FirstName", "Surname" },
+                values: new object[] { 1, true, "Jack", "Ferguson" });
+
+            migrationBuilder.InsertData(
+                schema: "thamco.events",
+                table: "Staffs",
+                columns: new[] { "StaffId", "FirstAider", "FirstName", "Surname" },
+                values: new object[] { 2, true, "John", "Boyd" });
+
+            migrationBuilder.InsertData(
+                schema: "thamco.events",
+                table: "Staffs",
+                columns: new[] { "StaffId", "FirstAider", "FirstName", "Surname" },
+                values: new object[] { 3, false, "James", "Francis" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Staffs",
+                schema: "thamco.events");
+        }
+    }
+}
